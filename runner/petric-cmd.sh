e4570f1fae434065fe327441a23713f214e674f5
Casper da Costa-Luis <casper.dcl@physics.org>
2024-09-26 20:56:31 +0000

    2h timeout

--- runner/petric-cmd.sh
+++ runner/petric-cmd.sh
@@ -41,7 +41,7 @@ test -x ./INSTALL/bin/gadgetron && ./INSTALL/bin/gadgetron >& ~/gadgetron.log&
 popd
 
 curl -fsSL https://raw.githubusercontent.com/SyneRBI/PETRIC/main/petric.py > petric.py
-python -B petric.py
+timeout 2h python -B petric.py
 
 echo "stopping jobs"
 for i in $(jobs -p); do kill -n 15 $i; done 2>/dev/null
