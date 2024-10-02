552a3fdc45919a95a995c9a0bb0767d06fcc069f
Casper da Costa-Luis <casper.dcl@physics.org>
2024-10-02 13:13:33 +0000

    lint

--- runner/thresholds.py
+++ runner/thresholds.py
@@ -10,7 +10,7 @@ tb = SummaryWriter(logdir="/o/logs/0_THRESHOLDS")
 voi_names = {i.stem[4:] for i in Path("/mnt/share/petric").glob("*/PETRIC/VOI_*.hv")}
 t = int(time())
 
-for tic in (0, 10*60):
+for tic in (0, 10 * 60):
     for i in voi_names - {'whole_object', 'background'}:
         tb.add_scalar(f"AEM_VOI_{i}", QualityMetrics.THRESHOLD["AEM_VOI"], tic, t + tic)
     tb.add_scalar("RMSE_whole_object", QualityMetrics.THRESHOLD["RMSE_whole_object"], tic, t + tic)
