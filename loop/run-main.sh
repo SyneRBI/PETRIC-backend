1cbe500ef43fb5d4fd11a5e38f96322cecc1b719
Casper da Costa-Luis <casper.dcl@physics.org>
2026-01-22 14:35:13 +0000

    slight configurability increase

--- loop/run-main.sh
+++ loop/run-main.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -euo pipefail
 curl -fsSLO https://raw.githubusercontent.com/SyneRBI/PETRIC2/main/petric.py
-PYTHONPATH="$PWD${PYTHONPATH:+:$PYTHONPATH}" PETRIC_SKIP_DATA=1 python -B ../petric-run.py
+PYTHONPATH="$PWD${PYTHONPATH:+:$PYTHONPATH}" PETRIC_SKIP_DATA=1 python -B "$@"
--- loop/run.sh
+++ loop/run.sh
@@ -27,10 +27,11 @@ for sub in "${submissions[@]}" ; do
   docker run --rm --gpus all -u root -e NB_GID --no-healthcheck -e GITHUB_REPOSITORY=SyneRBI/PETRIC2-$team -e GITHUB_REF_NAME=$(git -C PETRIC describe --tags) \
     -e TQDM_MININTERVAL=1 \
     -e NUMEXPR_MAX_THREADS=24 \
-    -v /mnt/share-public/petric/2:/mnt/share/petric:ro \
+    -e PETRIC_SRCDIR=/mnt/share/petric/2 \
+    -v /mnt/share-public/petric:/mnt/share/petric:ro \
     -v /opt/runner/logs/2:/logs:rw \
     -v .:/w:rw -w /w/PETRIC \
-    ghcr.io/synerbi/sirf:petric2 \
-    /w/run-main.sh
+    ghcr.io/synerbi/sirf:petric2 /w/run-main.sh \
+    ../petric-run.py
   git -C PETRIC restore .
 done
